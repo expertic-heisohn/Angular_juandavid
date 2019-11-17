1ab7f6799b67e28a7559a19fce6a99a1c91922b0
Juanthe7 <juanthe7@hotmail.com>
2019-11-17 18:46:26 -0500

    avance de la entrega hasta: 7: Emit and handle events

---
+++ src/app/components/event/event.component.ts
@@ -0,0 +1,12 @@
+import {Component, Input} from '@angular/core';
+//import {Component} from '@angular/core';
+@Component({
+  selector: 'app-event',
+  templateUrl: './event.component.html',
+})
+
+
+export class EventComponent {
+    @Input() value: any;
+  }
+  
